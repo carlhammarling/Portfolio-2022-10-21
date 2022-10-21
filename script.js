9318f61ba2bb5e1652b283f602a32ef73ad4faa2
Carl Hammarling <carlhammarling@gmail.com>
2022-10-21 21:18:37 +0200

    message funktion

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
 const form = document.querySelector('#contact');
 
+const resetMsg = document.querySelector('#resetMsg')
 
+const valMsg = (id) => {
+    if (id.value.length < 4) {
+        console.log('Message too short.');
+        return false;
+    }
+    else {
+        return true;
+    }
+};
 
+const valName = (id) => {
+    if (id.value.length < 2) {
+        console.log('Name has to be at least 2 characters');
+        return false;
+    }
+    else {
+        return true;
+    }
+};
 
+const valEmail = (id) => {
+    if (id.value.length < 2) {
+        console.log('Email to short');
+        return false;
+    }
+    else {
+        return true;
+    }
+};
+
+//skapar ett meddelande
 class Message {
     constructor(message, name, email) {
         this.message = message.value
@@ -11,6 +41,9 @@ class Message {
     }
 }
 
+//errors
+const errors = [];
+//sparar meddelanden
 const messages = [];
 
 form.addEventListener('submit', e => {
@@ -20,9 +53,41 @@ form.addEventListener('submit', e => {
     const name = document.querySelector('#name');
     const email = document.querySelector('#email');
 
+    errors[0] = valMsg(message);
+    errors[1] = valName(name);
+    errors[2] = valEmail(email);
+
+    if (errors.includes(false)) {
+        console.log('Please fill in all the forms.')
+    }
+
+    else {
+    //skapar meddelande    
     const request = new Message(message, name, email);
-    
+    //pushar in meddelande i array
     messages.push(request);
 
+    //töm inputs
+    message.value = '';
+    name.value = '';
+    email.value = '';
+
+
+    //Gömmer formuläret o skickar ett meddelande att allt gick bra
+    form.classList.add('hide');
+    success.classList.remove('hide');
+
+
+    //loggar meddelandena    
     console.log(messages);
+    }
+})
+
+
+//
+resetMsg.addEventListener('submit', e => {
+    e.preventDefault();
+
+    form.classList.remove('hide');
+    success.classList.add('hide');
 })
